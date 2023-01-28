c55881a1f6b8e499be58514d3feb04864d275a04
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-01-28 16:32:38 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ connectToMongo();
 
 app.use(express.json());
 app.use(cors());
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/stocks',require('./routes/stockOps'))
 
 app.get(`/`, (req, res) => {
     res.send(`Testing!`);
